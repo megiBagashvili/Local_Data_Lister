50a371b245264ac8a7dd4f9ed4ad63851ad11009
nika <m2738227@gmail.com>
2025-06-21 17:38:37 +0400

    1.1 Initialize database and dependencies

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -12,9 +12,8 @@ app.use(cors());
 
 app.use('/api', localItemsRouter);
 
-// commented for testing 
-// app.listen(PORT, () => {
-//   console.log(`[Server] Server is running on http://localhost:${PORT}`);
-// });
+app.listen(PORT, () => {
+  console.log(`[Server] Server is running on http://localhost:${PORT}`);
+});
 
 export default app;
