b291ef9b1e17e0ac612bc9798b6a5efc2308af52
nika <m2738227@gmail.com>
2025-06-21 22:42:09 +0400

    Auth-1: Install Dependencies and scaffold auth modules

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import localItemsRouter from './routes/localItemsRouter';
+import localItemsRouter from './api/localItemsRouter';
 import dotenv from 'dotenv';
 
 dotenv.config();
