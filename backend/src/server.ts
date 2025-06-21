9d6efed15be9f7062e99de2e2842d8ea481e9ecc
nika <m2738227@gmail.com>
2025-06-21 16:04:02 +0400

    4. Implement basic configuration management.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import cors from 'cors';
 import localItemsRouter from './routes/localItemsRouter';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 
